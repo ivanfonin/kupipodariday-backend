98536b085e91592aff9aa88fb2d1ce1fd56c89ea
Ivan Fonin <ivanfonin@yandex.ru>
2024-07-28 22:07:20 +0300

    feat: Добавить подключение к БД при помощи TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import configuration from './configuration';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database'),
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/configuration.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME || 'student',
+    password: process.env.DATABASE_PASSWORD || 'student',
+    database: process.env.DATABASE_NAME || 'kupipodariday',
+    entities: [],
+    synchronize: false,
+  },
+});
