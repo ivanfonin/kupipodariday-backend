fe20c8a20823bac67e192d0125ca68f4d4f25fa8
Ivan Fonin <ivanfonin@yandex.ru>
2024-07-28 23:52:16 +0300

    refactor: добавить валидацию имени пользователя

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,18 @@
-import { IsEmail, IsOptional, IsString, IsUrl, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+  Matches,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
   @Length(2, 30)
+  @Matches(/^[a-zA-Z0-9_]+$/, {
+    message: 'Имя пользователя может содеражать только буквы, цифры и знак _',
+  })
   username: string;
 
   @IsOptional()
@@ -17,5 +27,6 @@ export class CreateUserDto {
   email: string;
 
   @IsString()
+  @Length(6)
   password: string;
 }
