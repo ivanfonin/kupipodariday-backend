7cd3d4089e803348c8e733a36b3068e0ae32456e
Ivan Fonin <ivanfonin@yandex.ru>
2024-07-29 16:31:46 +0300

    feat: Установить в проект nestjs/jwt и импортировать нужные модули в AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule, JwtModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
