8581c38f28d159c4d1cfc60edc1906fe01856993
Ivan Fonin <ivanfonin@yandex.ru>
2024-07-29 10:15:34 +0300

    feat: Добавить схему данных для сущности offer и импотировать все в соответствующие модули

--- src/offers/entities/offer.entity.ts
+++ src/offers/entities/offer.entity.ts
@@ -1,1 +1,20 @@
-export class Offer {}
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from 'src/utils/entities/base.entity';
+
+@Entity()
+export class Offer extends BaseEntity {
+  // user
+
+  // item
+
+  @Column({
+    type: 'numeric',
+    precision: 10,
+    scale: 2,
+    default: 0,
+  })
+  amount: number;
+
+  @Column({ default: false })
+  hidden: boolean;
+}
