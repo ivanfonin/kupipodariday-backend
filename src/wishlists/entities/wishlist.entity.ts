1648e0e39a756edcc62f62935af8920e1c9eb18c
Ivan Fonin <ivanfonin@yandex.ru>
2024-07-29 10:07:34 +0300

    feat: Добавить схему данных для сущности wishlist

--- src/wishlists/entities/wishlist.entity.ts
+++ src/wishlists/entities/wishlist.entity.ts
@@ -1,1 +1,20 @@
-export class Wishlist {}
+import { IsUrl, Length } from 'class-validator';
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from 'src/utils/entities/base.entity';
+
+@Entity()
+export class Wishlist extends BaseEntity {
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @Length(1, 1500)
+  description: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  // items
+}
