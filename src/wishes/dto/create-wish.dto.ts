61f9467f098ac9747d0d39654c1916bec8a4f50a
Ivan Fonin <ivanfonin@yandex.ru>
2024-08-04 17:40:53 +0300

    feat: Описать createWishDto

--- src/wishes/dto/create-wish.dto.ts
+++ src/wishes/dto/create-wish.dto.ts
@@ -1,1 +1,21 @@
-export class CreateWishDto {}
+import { IsNumber, IsPositive, IsString, IsUrl, Length } from 'class-validator';
+
+export class CreateWishDto {
+  @IsString()
+  @Length(2, 250)
+  name: string;
+
+  @IsUrl()
+  link: string;
+
+  @IsUrl()
+  image: string;
+
+  @IsPositive()
+  @IsNumber()
+  price: number;
+
+  @IsString()
+  @Length(1, 1024)
+  description: string;
+}
