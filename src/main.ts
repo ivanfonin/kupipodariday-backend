412efe788e6108fd0337c7f28775dacb51bca402
Ivan Fonin <ivanfonin@yandex.ru>
2024-08-05 16:32:47 +0300

    refactor Скрыть код для отладки перед отправкой на проверку

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Включить CORS для теста
-  app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
-
-  // Вывод в консоль всех  раутов для разработки
-  const server = app.getHttpServer();
-  const router = server._events.request._router;
-  const availableRoutes: Array<object> = router.stack
-    .filter((layer: any) => layer.route)
-    .map((layer: any) => ({
-      path: layer.route?.path,
-      method: layer.route?.stack[0].method,
-    }));
-  console.log(availableRoutes);
 }
 bootstrap();
