3ffa3a5fc269aeb80b3c11345c636091fd533a2b
Ivan Fonin <ivanfonin@yandex.ru>
2024-08-05 04:31:02 +0300

    refactor: Убрать модуль Wishes из Offers за ненадобностью на данный момент

--- src/offers/offers.service.ts
+++ src/offers/offers.service.ts
@@ -9,14 +9,12 @@ import { CreateOfferDto } from './dto/create-offer.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Offer } from './entities/offer.entity';
 import { DataSource, Repository } from 'typeorm';
-import { WishesService } from 'src/wishes/wishes.service';
 import { Wish } from 'src/wishes/entities/wish.entity';
 
 @Injectable()
 export class OffersService {
   constructor(
     @InjectRepository(Offer) private offerRepository: Repository<Offer>,
-    private readonly wishesService: WishesService,
     private readonly dataSource: DataSource,
   ) {}
 
